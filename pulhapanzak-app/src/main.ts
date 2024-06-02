fdfd148fff712cbbe1d2f9a0d7ac5cd48f8bf8ee
Figueb <131420933+Figueb@users.noreply.github.com>
2024-06-02 16:21:26 -0600

    commit login page

--- pulhapanzak-app/src/main.ts
+++ pulhapanzak-app/src/main.ts
@@ -1,11 +1,19 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 if (environment.production) {
   enableProdMode();
@@ -16,5 +24,21 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'pulha-app-9fbcd',
+          appId: '1:1071094746175:web:96f6affc55c60bc681fc8a',
+          storageBucket: 'pulha-app-9fbcd.appspot.com',
+          apiKey: 'AIzaSyAVzpRQ_boM8uDhJVT1LiN1kY8dVwkLYdY',
+          authDomain: 'pulha-app-9fbcd.firebaseapp.com',
+          messagingSenderId: '1071094746175',
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideMessaging(() => getMessaging())),
+    importProvidersFrom(provideStorage(() => getStorage())),
   ],
 });
